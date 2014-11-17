4c5047fd237af77ed4150e720a27ecbd7d1bcfb4
Gary Hammell <ghammell1@gmail.com>
2014-11-16 19:25:29 -0800

    add devise to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :subordinates, class_name: 'User', foreign_key: 'manager_id'
   belongs_to :manager, class_name: 'User'
 end
