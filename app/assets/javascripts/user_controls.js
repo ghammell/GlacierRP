44cd1522ed90619a4912e99a9731c13aec984bcb
Gary Hammell <ghammell1@gmail.com>
2014-11-25 00:56:35 -0800

    ajaxify log in and sign up forms

---
+++ app/assets/javascripts/user_controls.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+  BindUserControls('#log_in', '/users/sign_in')
+  BindUserControls('#sign_up', '/users/sign_up')
+})
+
+var BindUserControls = function(button, path) {
+  $(button).click( function() {
+    $.ajax({
+      method: 'GET',
+      url: path
+    })
+    .done(function(data){
+      $('.modal-content').empty()
+      $('.modal-content').prepend(data)
+      $('.modal').modal('show')
+    })
+  })
+}
